36f4afcdef2768c36d7fe1c10a7aa340e9d7a68c
Jonatan <jonatanav255@hotmail.com>
2025-11-21 15:14:58 -0600

    adding array list and list

--- src/main/java/com/bankrank/ui/LoginMenu.java
+++ src/main/java/com/bankrank/ui/LoginMenu.java
@@ -4,6 +4,8 @@ import com.bankrank.auth.AuthenticationService;
 import com.bankrank.model.User;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
